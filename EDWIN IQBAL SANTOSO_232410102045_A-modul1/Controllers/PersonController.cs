627f711a92dbcfad326ca565bb060d79cbebe447
Edwin Iqbal Santoso <164601790+Edwin811@users.noreply.github.com>
2025-03-12 08:13:03 +0700

    Add files via upload

---
+++ EDWIN IQBAL SANTOSO_232410102045_A-modul1/Controllers/PersonController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using percobaan2.Models;
+
+namespace percobaan2.Controllers
+{
+    public class PersonController : Controller
+    {
+        [HttpGet("api/person")]
+        public IActionResult GetAction(int id_person)
+        {
+            PersonContext context = new PersonContext();
+            List<Person> ListPerson = context.ListPerson();
+
+            Person Cari = null;
+
+            foreach (Person i in ListPerson)
+            {
+                if (i.id_person == id_person)
+                {
+                    Cari = i;
+                    break;
+                }
+            }
+
+            if (Cari == null)
+            {
+                return NotFound("id orang tersebut tidak ditemukan");
+            }
+
+            return Ok(Cari);
+        }
+
+    }
+}
